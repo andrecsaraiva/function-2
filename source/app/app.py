e50f135c824c1873c417f4ff0901b55246e574ed
andrecsaraiva <andrecarneirosaraiva89@gmail.com>
2026-03-28 20:16:12 -0300

    Primeira versão da function hello world

---
+++ source/app/app.py
@@ -0,0 +1,10 @@
+from flask import jsonify
+
+def run(request):
+    if request.method != "POST":
+        return jsonify({"error": "Use POST"}), 405
+
+    payload = request.get_json(silent=True) or {}
+    name = payload.get("name", "World")
+
+    return jsonify({"message": f"Hello {name}"}), 200
---
+++ source/main.py
@@ -0,0 +1,4 @@
+from app.app import run
+
+def main(request):
+    return run(request)
